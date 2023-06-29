2d4dbc44d85ec937cd9ee6b6f44a1cf47adb100d
Vaclav Bily <xbily@fi.muni.cz>
2023-06-29 14:54:05 +0200

    initial commit

---
+++ Runtime/DescriptionBaseSO.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Xprees.Core
+{
+    /// Base class for ScriptableObjects with a public description field visible only in Editor.
+    public class DescriptionBaseSO : ScriptableObject, IResettable
+    {
+#if UNITY_EDITOR
+        [Tooltip("Description of the ScriptableObject. Editor only.")]
+        [TextArea]
+        public string description;
+#endif
+
+        public virtual void ResetState()
+        {
+        }
+    }
+}
---
+++ Runtime/IResettable.cs
@@ -0,0 +1,8 @@
+﻿namespace Xprees.Core
+{
+    public interface IResettable
+    {
+        /// Reset the internal state of the object.
+        public void ResetState();
+    }
+}
