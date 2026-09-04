188717dc3e66082229f0bb3cff5bb1c1a51a365b
Vaclav Bily <xprees.developer@gmail.com>
2026-09-04 14:17:43 +0200

    FIX ungiarded UnitEditor using in Runtime

--- Runtime/StateLifetimeExtensions.cs
+++ Runtime/StateLifetimeExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Reflection;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Xprees.Core
 {
